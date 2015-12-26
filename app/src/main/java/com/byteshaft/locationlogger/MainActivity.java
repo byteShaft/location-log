8f0c2512ee7ba300dd7734fe4b0a0a8449da6f0a
Imran <linuxholic@hotmail.com>
2015-12-27 00:04:51 +0500

    Cancel timer on back press

--- app/src/main/java/com/byteshaft/locationlogger/MainActivity.java
+++ app/src/main/java/com/byteshaft/locationlogger/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.CountDownTimer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
             @Override
             public void onTick(long millisUntilFinished) {
+                Log.i("Timer", "Running");
                 if (mLocation != null) {
                     System.out.println(mLocation.getLatitude() + "\n" + mLocation.getLongitude());
                 }
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
             @Override
             public void onFinish() {
+                Log.i("Timer", "Finished");
                 if (mGoogleApiClient.isConnected()) {
                     stopLocationUpdates();
                 }
@@ -48,6 +51,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        if (timer != null) {
+            timer.cancel();
+            Log.i("Timer", "cancelled");
+        }
         if (mGoogleApiClient.isConnected()) {
             stopLocationUpdates();
         }
